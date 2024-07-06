c1499aa68f2179a24ce54a2cb6589e3bdb057ffb
ShammasAmir <amirshammas@gmail.com>
2024-07-06 11:53:24 +0330

    Partialize the home page template.

---
+++ resources/views/front/partials/header.blade.php
@@ -0,0 +1,43 @@
+<header>
+    <nav class="navbar navbar-expand-lg">
+      <div class="container-lg">
+  
+        <div class="hamburger-menu">
+          <i class="fas fa-bars"></i>
+        </div>
+  
+        
+          <ul class="navbar-nav">
+            <div class="close-menu">
+              <i class="fas fa-times"></i>
+            </div>
+            <li class="nav-item">
+              <a class="nav-link active" href="#">خانه</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">درباره ما</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">خدمات</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">پروژه ها</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">بلاگ</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">تماس با ما</a>
+            </li>
+  
+          </ul>
+       
+  
+        <div class="logo">
+          <a href="">Modern-Saze</a>
+        </div>
+      </div>
+    </nav>
+  
+    <div class="overlay-menu"></div>
+  </header>
