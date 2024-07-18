1e8c7c865f6ebfaa87e8e3c686f238b0eff3b141
ShammasAmir <amirshammas@gmail.com>
2024-07-18 21:41:01 +0330

    Add is-active class to sidebar.

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -1,67 +1,73 @@
 <div id="mySlidenav" class="slidenav">
-    <p class="logo"><span>M</span>odern-Saze</p>
+    <p class="logo"><span>M</span>odern-<span>S</span>aze</p>
     <a href="{{route('home')}}" class="icon-a">
         <span class="icon"><i class="fas fa-eye"></i></span>
         نمایش وب سایت
     </a>
-    <a href="{{route('dashboard')}}" class="icon-a">
+    <a href="{{route('dashboard')}}" class="icon-a @if(request()->is('dashboard')) is-active" @endif>
         <span class="icon"><i class="fas fa-home"></i></span>
         داشبورد
     </a>
-    <a href="#" class="icon-a">
-        <span class="icon"><i class="fas fa-ellipsis-h"></i></span>
-        منوی بالایی
-    </a>
-    <a href="#" class="icon-a">
-        <span class="icon"><i class="fas fa-laptop-house"></i></span>
-        خانه
-    </a>
-    <a href="#" class="icon-a">
-        <span class="icon"><i class="fas fa-address-card"></i></span>
-        درباره ما
-    </a>
-    <a href="#" class="icon-a">
-        <span class="icon"><i class="fas fa-book-open"></i></span>
-        مقدمه
-    </a>
-    <a href="#" class="icon-a">
-        <span class="icon"><i class="fas fa-cogs"></i></span>
-        خدمات
-    </a>
-    <a href="#" class="icon-a">
-        <span class="icon"><i class="fas fa-user-friends"></i></span>
-        تیم
-    </a>
-    <a href="#" class="icon-a">
-        <span class="icon"><i class="fas fa-images"></i></span>
-        نمونه کارها
-    </a>
-    <a href="#" class="icon-a">
-        <span class="icon"><i class="fas fa-comment-alt"></i></span>
-        نظرات مشتریان
-    </a>
-    <a href="#" class="icon-a">
-        <span class="icon"><i class="fas fa-headset"></i></span>
-        سوالات متداول
-    </a>
-    <a href="#" class="icon-a">
-        <span class="icon"><i class="fas fa-clipboard-list"></i></span>
-        فوتر
-    </a>
-    <a href="{{route('users.index')}}" class="icon-a">
-        <span class="icon"><i class="fas fa-users"></i></span>
-        کاربران
-    </a>
-    <a href="#" class="icon-a">
-        <span class="icon"><i class="fas fa-comment"></i></span>
-        نظرات
-    </a>
-    <a href="#" class="icon-a">
-        <span class="icon"><i class="fas fa-users"></i></span>
-        پیام کاربران
-    </a>
-    <a href="#" class="icon-a">
-        <span class="icon"><i class="fas fa-blog"></i></span>
-        بلاگ
-    </a>
+
+    @if(auth()->user()->role === 'admin')
+        <a href="#" class="icon-a">
+            <span class="icon"><i class="fas fa-ellipsis-h"></i></span>
+            منوی بالایی
+        </a>
+        <a href="#" class="icon-a">
+            <span class="icon"><i class="fas fa-laptop-house"></i></span>
+            خانه
+        </a>
+        <a href="#" class="icon-a">
+            <span class="icon"><i class="fas fa-address-card"></i></span>
+            درباره ما
+        </a>
+        <a href="#" class="icon-a">
+            <span class="icon"><i class="fas fa-book-open"></i></span>
+            مقدمه
+        </a>
+        <a href="#" class="icon-a">
+            <span class="icon"><i class="fas fa-cogs"></i></span>
+            خدمات
+        </a>
+        <a href="#" class="icon-a">
+            <span class="icon"><i class="fas fa-user-friends"></i></span>
+            تیم
+        </a>
+        <a href="#" class="icon-a">
+            <span class="icon"><i class="fas fa-images"></i></span>
+            نمونه کارها
+        </a>
+        <a href="#" class="icon-a">
+            <span class="icon"><i class="fas fa-comment-alt"></i></span>
+            نظرات مشتریان
+        </a>
+        <a href="#" class="icon-a">
+            <span class="icon"><i class="fas fa-headset"></i></span>
+            سوالات متداول
+        </a>
+        <a href="#" class="icon-a">
+            <span class="icon"><i class="fas fa-clipboard-list"></i></span>
+            فوتر
+        </a>
+        <a href="{{route('users.index')}}" class="icon-a @if(request()->is('dashboard/users')) is-active" @endif>
+            <span class="icon"><i class="fas fa-users"></i></span>
+            کاربران
+        </a>
+    @endif
+
+    @if((auth()->user()->role === 'admin') || (auth()->user()->role === 'author'))
+        <a href="#" class="icon-a">
+            <span class="icon"><i class="fas fa-comment"></i></span>
+            نظرات
+        </a>
+        <a href="#" class="icon-a">
+            <span class="icon"><i class="fas fa-users"></i></span>
+            پیام کاربران
+        </a>
+        <a href="#" class="icon-a">
+            <span class="icon"><i class="fas fa-blog"></i></span>
+            بلاگ
+        </a>
+    @endif
 </div>
